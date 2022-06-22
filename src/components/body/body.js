41bd9108a4d431f408a52591de066d30ca76bca0
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-06-22 14:45:01 +0800

    Add onAddNoteHandler

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -2,10 +2,10 @@ import React from 'react';
 import NoteInput from "./noteInput";
 import ListNotes from "./listNotes";
 
-function Body({ data }) {
+function Body({ data, onAddNoteHandler }) {
     return (
         <main className="note-app__body">
-            <NoteInput />
+            <NoteInput onAddNoteHandler={onAddNoteHandler} />
             <ListNotes title="Catatan Aktif" archiveTriggerText="Arsipkan"
                        data={data.filter((note) => !note.archived)} />
             <ListNotes title="Arsip" archiveTriggerText="Aktifkan" data={data.filter((note) => note.archived)} />
--- src/components/body/noteInput.js
+++ src/components/body/noteInput.js
@@ -1,15 +1,56 @@
 import React from "react";
+import body from "./body";
 
 class NoteInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            body: ""
+        };
+
+        this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+        this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+    }
+
+    onTitleChangeEventHandler(e) {
+        const newTitle = e.target.value;
+        const isValid = newTitle.length <= 50;
+
+        if (isValid) {
+            this.setState({
+                title: newTitle
+            })
+        }
+    }
+
+    onBodyChangeEventHandler(e) {
+        this.setState({
+            body: e.target.value
+        })
+    }
+
+    onSubmitEventHandler(e) {
+        e.preventDefault();
+        this.props.onAddNoteHandler(
+            this.state.title,
+            this.state.body
+        )
+    }
+
     render() {
         return (
             <div>
-                <form className="note-input">
+                <form className="note-input" onSubmit={this.onSubmitEventHandler}>
                     <h2>Buat catatan</h2>
-                    <p className="note-input__title__char-limit">Sisa karakter: 50</p>
-                    <input className="note-input__title" placeholder="Judul" required={true}/>
-                    <textarea className="note-input__body" placeholder="Tulis catatan ..." required={true}/>
-                    <button>Buat</button>
+                    <p className="note-input__title__char-limit">Sisa karakter: {50 - this.state.title.length}</p>
+                    <input className="note-input__title" placeholder="Judul" required={true} value={this.state.title}
+                           onChange={this.onTitleChangeEventHandler} />
+                    <textarea className="note-input__body" placeholder="Tulis catatan ..." required={true}
+                              value={this.state.body} onChange={this.onBodyChangeEventHandler} />
+                    <button type="submit">Buat</button>
                 </form>
             </div>
         )
