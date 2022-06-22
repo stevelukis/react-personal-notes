074ce5035df956df8aa08abc1abb4e3633ac0ea1
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-06-22 10:37:05 +0800

    Add searchBox

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBox from "./searchBox";
 
 function Header() {
     return (
         <header className="note-app__header">
             <h1>Notes</h1>
+            <SearchBox />
         </header>
     )
 }
---
+++ src/components/header/searchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBox extends React.Component {
+    render() {
+        return (
+            <div>
+                <input placeholder="Cari catatan ..." />
+            </div>
+        );
+    }
+}
+
+export default SearchBox;
