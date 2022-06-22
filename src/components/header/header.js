6d15b28ebd2995988ea1d404b4bb3920b66c4f13
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-06-22 10:34:00 +0800

    Initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="note-app__header">
+            <h1>Notes</h1>
+        </header>
+    )
+}
+
+export default Header;
