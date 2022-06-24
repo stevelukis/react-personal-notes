2bae3cc99424ea65ea35cab06b96f9b668163137
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-06-24 15:28:12 +0800

    Add message if the list is empty

--- src/components/body/listNotes.js
+++ src/components/body/listNotes.js
@@ -2,19 +2,27 @@ import React from "react";
 import NoteCard from "./noteCard";
 
 function ListNotes({ title, archiveTriggerText, data, onDeleteHandler, onArchiveTriggerHandler }) {
+    let content;
+    if (data.length === 0) {
+        content = <p className="note-app__body__empty-message">Tidak ada catatan</p>;
+    } else {
+        content = <div className="notes-list">
+            {
+                data.map((note) => (
+                    <NoteCard key={note.id} id={note.id} title={note.title} body={note.body}
+                              timestamp={note.createdAt}
+                              archiveTriggerText={archiveTriggerText} onDeleteHandler={onDeleteHandler}
+                              onArchiveTriggerHandler={onArchiveTriggerHandler} />
+                ))
+            }
+        </div>
+
+    }
+
     return (
         <>
             <h2>{title}</h2>
-            <div className="notes-list">
-                {
-                    data.map((note) => (
-                        <NoteCard key={note.id} id={note.id} title={note.title} body={note.body}
-                                  timestamp={note.createdAt}
-                                  archiveTriggerText={archiveTriggerText} onDeleteHandler={onDeleteHandler}
-                                  onArchiveTriggerHandler={onArchiveTriggerHandler} />
-                    ))
-                }
-            </div>
+            {content}
         </>
     );
 }
